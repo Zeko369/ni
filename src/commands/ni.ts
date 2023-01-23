a613a7d0a2cc0fcd648b40afd5f6b9194e9b77be
Fran Zekan <zekan.fran369@gmail.com>
2023-01-23 10:51:54 +0100

    wip

--- src/commands/ni.ts
+++ src/commands/ni.ts
@@ -1,4 +1,14 @@
+import { getConfig } from '../config'
 import { parseNi } from '../parse'
 import { runCli } from '../runner'
 
-runCli(parseNi)
+runCli(async (agent, args, ctx) => {
+  if (args.includes('--config')) {
+    const config = await getConfig()
+    console.log(JSON.stringify(config, null, 2))
+
+    return ''
+  }
+
+  return parseNi(agent, args, ctx)
+})
--- src/config.ts
+++ src/config.ts
@@ -26,32 +26,43 @@ const defaultConfig: Config = {
 }
 
 let config: Config | undefined
+let loadedFrom: 'pkg' | 'rc' | 'default' | 'none' = 'none'
 
-export async function getConfig(): Promise<Config> {
+export async function getConfig() {
   if (!config) {
-    const result = await findUp('package.json') || ''
+    const result = (await findUp('package.json')) || ''
     let packageManager = ''
     if (result)
       packageManager = JSON.parse(fs.readFileSync(result, 'utf8')).packageManager ?? ''
+
     const [, agent, version] = packageManager.match(new RegExp(`^(${Object.values(LOCKS).join('|')})@(\d).*?$`)) || []
-    if (agent)
-      config = Object.assign({}, defaultConfig, { defaultAgent: (agent === 'yarn' && parseInt(version) > 1) ? 'yarn@berry' : agent })
-    else if (!fs.existsSync(rcPath))
+    if (agent) {
+      config = Object.assign({}, defaultConfig, {
+        defaultAgent: agent === 'yarn' && parseInt(version) > 1 ? 'yarn@berry' : agent,
+      })
+      loadedFrom = 'pkg'
+    }
+    else if (!fs.existsSync(rcPath)) {
       config = defaultConfig
-    else
+      loadedFrom = 'default'
+    }
+    else {
       config = Object.assign({}, defaultConfig, ini.parse(fs.readFileSync(rcPath, 'utf-8')))
+      loadedFrom = 'rc'
+    }
   }
-  return config
+
+  return [config, loadedFrom] as const
 }
 
 export async function getDefaultAgent() {
-  const { defaultAgent } = await getConfig()
+  const [{ defaultAgent }] = await getConfig()
   if (defaultAgent === 'prompt' && process.env.CI)
     return 'npm'
   return defaultAgent
 }
 
 export async function getGlobalAgent() {
-  const { globalAgent } = await getConfig()
+  const [{ globalAgent }] = await getConfig()
   return globalAgent
 }
